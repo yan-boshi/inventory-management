5435869793be1e34debf0d7f63b2c7012b1a4afd
yan-boshi <1327764469@qq.com>
2026-07-06 21:35:16 +0800

    fix： build时隐藏gzip大小输出

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
+  build: {
+    reportCompressedSize: false,
+  },
   server: {
     port: 5173,
     proxy: {
